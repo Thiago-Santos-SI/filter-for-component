bbb5cce17dcf1744cd75f4b0ae03e97997caf568
Thiago-Santos-SI <thiiagosantos300@gmail.com>
2021-03-23 20:56:25 -0300

    still working on focus typescript

--- src/filter.typescript.ts
+++ src/filter.typescript.ts
@@ -30,35 +30,13 @@ export const removeAccentsSpace = (str: string): string => {
         .toLowerCase();
 };
 
-export const filterComponentReset = (
-    featuredCategories: HomePageFeaturedCategoryResponse[],
-    values: Values,
-    callBackParentValuesFiltered: (featuredCategories: HomePageFeaturedCategoryResponse[]) => void,
-): void => {
-    if (!localStorage.getItem('items')) {
-        localStorage.setItem('items', JSON.stringify(featuredCategories));
-    }
-
-    const filterComponentReponse = filterComponent(JSON.parse(localStorage.getItem('items')), values);
-
-    if (!filterComponentReponse.click) {
-        callBackParentValuesFiltered(filterComponentReponse.featuredCategoriesFiltered);
-    } else {
-        callBackParentValuesFiltered(JSON.parse(localStorage.getItem('items')));
-    }
-};
-
 export const filterComponent = (
     featuredCategories: HomePageFeaturedCategoryResponse[],
     values: Values,
 ): FilterComponentResponse => {
-    const arrayManipulated = [];
+    let arrayManipulated = [];
 
     const ExistingFeaturedCategories = featuredCategories.filter(category => category.slider.events.length);
-
-    const date = { from: '2021-03-18', to: '2022-03-27' };
-
-    values.date = date;
 
     values.categories.map(category => {
         const categories = ExistingFeaturedCategories.filter(item => item.slider.categorySlug === category);
@@ -77,8 +55,7 @@ export const filterComponent = (
     });
 
     arrayManipulated.map((array, index) => {
-        const LISTA_DE_DATAS = [];
-        const LISTA_DE_DATAS_FINAL = [];
+        const elementsBeforeDate = [];
 
         array.slider.events
             .filter(item => item.date)
@@ -88,11 +65,13 @@ export const filterComponent = (
                 const dateBefore = isBefore(parseISO(dateFormattedBefore), parseISO(values.date.from));
 
                 if (!dateBefore) {
-                    LISTA_DE_DATAS.push(item);
+                    elementsBeforeDate.push(item);
                 }
             });
 
-        LISTA_DE_DATAS.filter(date => {
+        const elementsBeforeEndDate = [];
+
+        elementsBeforeDate.filter(date => {
             const dateFormattedLater = format(parseISO(date.date), 'yyyy-MM-dd');
 
             const dateLater = isBefore(
@@ -103,34 +82,49 @@ export const filterComponent = (
             );
 
             if (dateLater) {
-                LISTA_DE_DATAS_FINAL.push(date);
+                elementsBeforeEndDate.push(date);
             }
         });
-        arrayManipulated[index].slider.events = LISTA_DE_DATAS_FINAL;
+        arrayManipulated[index].slider.events = elementsBeforeEndDate;
     });
 
-    //TESTE -> arr[0].slider.events[2].location = 'Rio de Janeiro, RJ'
+    // -> arr[0].slider.events[2].location = 'Rio de Janeiro, RJ'
+
+    if (arrayManipulated.length === 0) {
+        arrayManipulated = ExistingFeaturedCategories;
+    }
 
     if (values.location) {
         if (values.location === 'sp') {
             arrayManipulated.map((item, index) => {
-                const LISTA_LOCAIS_EVENTOS = [];
+                const arrayEventsLocales = [];
 
                 item.slider.events.filter(event => {
                     const checkLocation = event.location === 'São Paulo, SP';
                     if (checkLocation) {
-                        LISTA_LOCAIS_EVENTOS.push(event);
+                        arrayEventsLocales.push(event);
                     }
                 });
-                arrayManipulated[index].slider.events = LISTA_LOCAIS_EVENTOS;
+                arrayManipulated[index].slider.events = arrayEventsLocales;
             });
         } else if (values.location === 'rj') {
+            arrayManipulated.map((item, index) => {
+                const arrayEventsLocales = [];
+
+                item.slider.events.filter(event => {
+                    const checkLocation = event.location === 'Rio de Janeiro, RJ';
+                    if (checkLocation) {
+                        arrayEventsLocales.push(event);
+                    }
+                });
+                arrayManipulated[index].slider.events = arrayEventsLocales;
+            });
         }
     }
 
     if (values.locationQuery) {
         arrayManipulated.map((item, index) => {
-            const LISTA_LOCAIS_EVENTOS = [];
+            const arraySearchEventsLocales = [];
 
             item.slider.events.filter(event => {
                 const locationQuery = removeAccentsSpace(values.locationQuery);
@@ -156,10 +150,10 @@ export const filterComponent = (
                 );
 
                 if (result[0] && result[1] && result[2] && result[3] && result[4]) {
-                    LISTA_LOCAIS_EVENTOS.push(event);
+                    arraySearchEventsLocales.push(event);
                 }
             });
-            arrayManipulated[index].slider.events = LISTA_LOCAIS_EVENTOS;
+            arrayManipulated[index].slider.events = arraySearchEventsLocales;
         });
     }
 
@@ -167,4 +161,23 @@ export const filterComponent = (
         featuredCategoriesFiltered: arrayManipulated,
         click: values.categories.length > 0 ? false : true,
     };
+};
+
+export const filterComponentReset = (
+    featuredCategories: HomePageFeaturedCategoryResponse[],
+    values: Values,
+    callBackParentValuesFiltered: (featuredCategories: HomePageFeaturedCategoryResponse[]) => void,
+): void => {
+    if (!localStorage.getItem('items')) {
+        localStorage.setItem('items', JSON.stringify(featuredCategories));
+    }
+
+    const filterComponentReponse = filterComponent(JSON.parse(localStorage.getItem('items')), values);
+
+    if (!filterComponentReponse.click) {
+        callBackParentValuesFiltered(filterComponentReponse.featuredCategoriesFiltered);
+    } else {
+        callBackParentValuesFiltered(filterComponentReponse.featuredCategoriesFiltered);
+        //callBackParentValuesFiltered(JSON.parse(localStorage.getItem('items')));
+    }
 };
